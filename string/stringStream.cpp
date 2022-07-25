1a69c041ac660c54f348a0356e3b465da3ee0117
Adityaverma <adityaverma4648@gmail.com>
2022-07-25 17:10:30 +0530

    addded fequencyOfWords Function

--- string/stringStream.cpp
+++ string/stringStream.cpp
@@ -18,17 +18,26 @@ int countWords(string str){
 //  printing frequency of occurance 
 
  void printFrequency(string str){
-      
+      map <string,int>FW;
+
+      stringstream ss(str);
+
+      string word;
+
+      while(ss >> word){
+            FW[word]++;
+      }
+
+      map<string , int>::iterator m;
+      for(m = FW.begin() ; m != FW.end() ; m++){
+              cout << m->first << "->" << m->second << endl;
+      }
  } 
 
 int main(){
    // word count
-string s = "geeks for geeks geeks "
-               "contribution placements";
-    cout << " Number of words are: " << countWords(s);
-
-
-
-
+string s = "geeks for geeks geeks contribution placements";
+    cout << " Number of words are: " << countWords(s) << endl;
+      printFrequency(s);
     return 0;
 }
